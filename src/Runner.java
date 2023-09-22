a703c9df0f05926d15e8127f0170f89da2a203b8
Pietro Ferrara <pietro.ferrara@unive.it>
2023-09-22 11:29:47 +0200

    hello world (lect. 2)

---
+++ src/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println(sum(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+    }
+
+    static int sum(int a, int b) {
+        return a+b;
+    }
+}
